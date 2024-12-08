7207aee2b3f101a33b1f12d97d8dc053e429d894
AlimightyYogi <nisaafriani957@gmail.com>
2024-12-08 20:14:54 +0700

    Update

--- config/db.js
+++ config/db.js
@@ -3,20 +3,20 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PORT,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME,      // Nama database
+    process.env.DB_USER,      // Username database
+    process.env.DB_PASSWORD,  // Password database
     {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        dialectModule: mysql2,
-        logging: false,
+        host: process.env.DB_HOST, // Host database
+        dialect: 'mysql',      // Dialek yang digunakan
+        dialectModule: mysql2, // Dialek MySQL
+        port: process.env.DB_PORT, // Port database
+        logging: false,       // Menonaktifkan logging query SQL
         pool: {
-            max: 5,
-            min: 0,
-            acquire: 60000,
-            idle: 10000,
+            max: 5,           // Maksimum koneksi dalam pool
+            min: 0,           // Minimum koneksi dalam pool
+            acquire: 60000,   // Waktu maksimal untuk mendapatkan koneksi
+            idle: 10000,      // Waktu idle sebelum koneksi di-terminate
         },
     }
 );

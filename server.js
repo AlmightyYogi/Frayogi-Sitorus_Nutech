5e1e0f1d94be024cda21a3282b9243be152ad8c6
AlimightyYogi <nisaafriani957@gmail.com>
2024-12-08 20:30:38 +0700

    Update

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const bannerRoutes = require('./routes/bannerRoutes');
 const serviceRoutes = require('./routes/serviceRoutes');
+
 const transactionRoutes = require('./routes/transactionRoutes');
 
 dotenv.config();
@@ -15,11 +16,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', authRoutes);
 app.use('/api', bannerRoutes);
 app.use('/api', serviceRoutes);
+
 app.use('/api', transactionRoutes);
 
 app.use('/uploads', express.static('uploads'));
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(4000, () => {
+    console.log('Server running on port 4000');
 });

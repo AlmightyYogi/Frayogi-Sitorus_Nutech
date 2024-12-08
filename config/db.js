595bb4e74735fbb3f305d7f8e864cf4ccc0f64ef
AlimightyYogi <nisaafriani957@gmail.com>
2024-12-08 22:13:06 +0700

    Update

--- config/db.js
+++ config/db.js
@@ -1,11 +1,18 @@
 const mysql = require('mysql2');
 
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'nutech'
+// });
+
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'nutech'
-});
+    host: 'mysql.railway.internal',
+    user: 'root',
+    password: 'bOWHkvIrTtxiNfYSYrFekksFnmSdFirN',
+    database: 'railway'
+  });
 
 db.connect(err => {
   if (err) {
